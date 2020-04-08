a614a5cf6e4cc71f07df3723be7c3daf795a02e1
Amirtha <amirtha4501@gmail.com>
2020-04-08 12:51:51 +0530

    Add Rxjs part 1

--- src/app/services/dish.service.ts
+++ src/app/services/dish.service.ts
@@ -2,7 +2,7 @@ import { Injectable } from '@angular/core';
 import { DISHES } from '../shared/dishes';
 import { Dish } from '../shared/dish';
 import { delay } from 'rxjs/operators';
-import { of } from 'rxjs';
+import { of, Observable } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
@@ -11,15 +11,15 @@ export class DishService {
 
   constructor() { }
 
-  getDishes(): Promise<Dish[]> {
-    return of(DISHES).pipe(delay(4000)).toPromise();
+  getDishes(): Observable<Dish[]> {
+    return of(DISHES).pipe(delay(2000));
   }
 
-  getDish(id: string): Promise<Dish> {
-    return of(DISHES.filter((dish) => (dish.id === id)) [0]).pipe(delay(4000)).toPromise();
+  getDish(id: string): Observable<Dish> {
+    return of(DISHES.filter((dish) => (dish.id === id)) [0]).pipe(delay(2000));
   }
 
-  getFeaturedDish(): Promise<Dish> {
-    return of(DISHES.filter((dish) => dish.featured) [0]).pipe(delay(4000)).toPromise();    
+  getFeaturedDish(): Observable<Dish> {
+    return of(DISHES.filter((dish) => dish.featured) [0]).pipe(delay(2000));    
   }
 }
