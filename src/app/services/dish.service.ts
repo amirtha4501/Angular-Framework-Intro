195ed374a73f1e1bc86ffe19696884694b69f7b6
Amirtha <amirtha4501@gmail.com>
2020-04-08 12:38:38 +0530

    Add Promise part 2.1

--- src/app/services/dish.service.ts
+++ src/app/services/dish.service.ts
@@ -1,6 +1,8 @@
 import { Injectable } from '@angular/core';
 import { DISHES } from '../shared/dishes';
 import { Dish } from '../shared/dish';
+import { delay } from 'rxjs/operators';
+import { of } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
@@ -10,21 +12,14 @@ export class DishService {
   constructor() { }
 
   getDishes(): Promise<Dish[]> {
-    return new Promise(resolve => {
-      setTimeout(() => resolve(DISHES), 4000);
-    });
+    return of(DISHES).pipe(delay(4000)).toPromise();
   }
 
   getDish(id: string): Promise<Dish> {
-    return new Promise(resolve => {
-      setTimeout(() => resolve(DISHES.filter((dish) => (dish.id === id)) [0]), 4000);
-    });
+    return of(DISHES.filter((dish) => (dish.id === id)) [0]).pipe(delay(4000)).toPromise();
   }
 
   getFeaturedDish(): Promise<Dish> {
-    return new Promise(resolve => {
-      setTimeout(() => resolve(DISHES.filter((dish) => dish.featured) [0]), 4000);
-    });
-    
+    return of(DISHES.filter((dish) => dish.featured) [0]).pipe(delay(4000)).toPromise();    
   }
 }
--- src/app/services/leader.service.ts
+++ src/app/services/leader.service.ts
@@ -1,6 +1,8 @@
 import { Injectable } from '@angular/core';
 import { Leader } from '../shared/leader';
 import { LEADERS } from '../shared/leaders';
+import { of } from 'rxjs';
+import { delay } from 'rxjs/operators';
 
 @Injectable({
   providedIn: 'root'
@@ -10,21 +12,15 @@ export class LeaderService {
   constructor() { }
 
   getLeaders(): Promise<Leader[]> {
-    return new Promise(resolve => {
-      setTimeout(() =>resolve(LEADERS), 4000);
-    });
+    return of(LEADERS).pipe(delay(4000)).toPromise();
   }
 
   getLeader(id: string): Promise<Leader> {
-    return new Promise(resolve => {
-      setTimeout(() => resolve(LEADERS.filter((lead) => (lead.id === id)) [0]), 4000);
-    });
+    return of(LEADERS.filter((lead) => (lead.id === id)) [0]).pipe(delay(4000)).toPromise();
   }
 
   getFeaturedLeader(): Promise<Leader> {
-    return new Promise(resolve => {
-      setTimeout(() => resolve(LEADERS.filter((lead) => lead.featured) [0]), 4000);
-    });
+    return of(LEADERS.filter((lead) => lead.featured) [0]).pipe(delay(4000)).toPromise();
   }
 
 }
