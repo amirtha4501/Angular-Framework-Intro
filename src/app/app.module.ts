8bbd2a89c559be5e76e4af5c6606a4fcde189a81
Amirtha <amirtha4501@gmail.com>
2020-04-09 19:19:11 +0530

    Add http part 1

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -17,9 +17,11 @@ import { ReactiveFormsModule } from '@angular/forms';
 import { MatSelectModule } from '@angular/material/select';
 import { MatSlideToggleModule } from '@angular/material/slide-toggle';
 import { MatProgressSpinnerModule } from '@angular/material/progress-spinner';
-import {MatSliderModule} from '@angular/material/slider';
+import { MatSliderModule } from '@angular/material/slider';
+import { HttpClientModule } from '@angular/common/http';
+import { HttpModule } from '@angular/http';
 
-import 'hammerjs';
+import 'hammerjs';``
 
 import { AppComponent } from './app.component';
 import { MenuComponent } from './menu/menu.component';
@@ -36,7 +38,8 @@ import { DishService } from './services/dish.service';
 import { PromotionService } from './services/promotion.service';
 import { LeaderService } from './services/leader.service';
 import { LoginComponent } from './login/login.component';
-import { from } from 'rxjs';
+
+import { baseURL } from './shared/baseurl';
 
 @NgModule({
   declarations: [
@@ -69,12 +72,19 @@ import { from } from 'rxjs';
     MatSelectModule,
     MatSlideToggleModule,
     MatProgressSpinnerModule,
-    MatSliderModule  
+    MatSliderModule,
+    HttpClientModule,
+    HttpModule  
   ],
   entryComponents: [
     LoginComponent
   ],
-  providers: [ DishService, PromotionService, LeaderService],
+  providers: [ 
+    DishService, 
+    PromotionService, 
+    LeaderService,
+    {provide: 'BaseURL', useValue: baseURL}
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
--- src/app/services/dish.service.ts
+++ src/app/services/dish.service.ts
@@ -1,29 +1,38 @@
 import { Injectable } from '@angular/core';
-import { DISHES } from '../shared/dishes';
 import { Dish } from '../shared/dish';
 import { delay } from 'rxjs/operators';
 import { of, Observable } from 'rxjs';
-
+import { HttpClient } from '@angular/common/http';
+import { baseURL } from '../shared/baseurl';
+import { map } from 'rxjs/operators';
+ 
 @Injectable({
   providedIn: 'root'
 })
 export class DishService {
 
-  constructor() { }
+  constructor(
+    private http: HttpClient
+  ) { }
 
   getDishes(): Observable<Dish[]> {
-    return of(DISHES).pipe(delay(2000));
+    return this.http.get<Dish[]>(baseURL + 'dishes');
+    // of(DISHES).pipe(delay(2000));
   }
 
   getDish(id: string): Observable<Dish> {
-    return of(DISHES.filter((dish) => (dish.id === id)) [0]).pipe(delay(2000));
+    return this.http.get<Dish>(baseURL + 'dishes/' + id)
+    // of(DISHES.filter((dish) => (dish.id === id)) [0]).pipe(delay(2000));
   }
 
   getFeaturedDish(): Observable<Dish> {
-    return of(DISHES.filter((dish) => dish.featured) [0]).pipe(delay(2000));    
+    return this.http.get<Dish[]>(baseURL + 'dishes?featured=true')
+      .pipe(map(dishes => dishes[0]));
+    // of(DISHES.filter((dish) => dish.featured) [0]).pipe(delay(2000));    
   }
 
   getDishIds(): Observable<string[] | any> {
-    return of(DISHES.map(dish => dish.id ));
+    return this.getDishes().pipe(map(dishes => dishes.map(dish => dish.id )));
+    // of(DISHES.map(dish => dish.id ));
   }
 }
